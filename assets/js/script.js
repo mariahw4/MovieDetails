a8a5b04dbd245b493ce05b5b7aa9e0af0ce1aca3
Jorge Gomez <jorge.electron@gmail.com>
2023-01-30 18:42:53 -0800

    Adds calls to API and displays info in app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
  // Global variables
- var movieList = [];
- var movieName;
+var movieList = [];
+var movieName;
+var mainActor;
 
 // local storage functions
 initMovieList();
@@ -21,8 +22,8 @@ function renderMovies(){
 // This function pulls the movie list array from local storage
 function initMovieList() {
     var storedMovies = JSON.parse(localStorage.getItem("movies"));
-    
-    if (storedMovies !== null) {
+
+        if (storedMovies !== null) {
         movieList = storedMovies;
     }
     
@@ -34,7 +35,6 @@ function initWikipedia() {
 
     if (storedWikipedia !== null) {
         movieName = storedWikipedia;
-
         displayWikipedia();
     }
 }
@@ -46,54 +46,107 @@ function storeMovieArray() {
 
 // This function saves the currently displayed movie to local storage
 function storeCurrentMovie() {
-
     localStorage.setItem("currentMovie", JSON.stringify(movieName));
 }
-      
+
 // Click event handler for movie search button
 $(".search-btn").on("click", function(event){
     event.preventDefault();
-
-    movieName = $("#movie-name").val().trim();
-    if(movieName === ""){
+    var userMovieName;
+    userMovieName = $("#movie-name").val().trim();
+    if(userMovieName === ""){
         alert("Please enter a movie to look up")
 
     }else if (movieList.length >= 3){  
         movieList.shift();
-        movieList.push(movieName);
+        movieList.push(userMovieName);
 
     }else{
-    movieList.push(movieName);
+    movieList.push(userMovieName);
     }
     storeCurrentMovie();
     storeMovieArray();
     renderMovies();
     $('#PosterContainer').empty();
-    callOmdb(movieName);
-    // displayWikipedia();
+    getMovieDetails(userMovieName);
+        
 });
 
-function callOmdb(queryString){
-let url = 'https://www.omdbapi.com/?t=' + queryString + '=&apikey=b05ca673';
+function getMovieDetails(movie){
+    let url = 'https://www.omdbapi.com/?t=' + movie + '=&apikey=b05ca673';
 
-fetch(url)
-.then(function (response){
-  return(response.json());
-}).then(function(data){
+    fetch(url)
+    .then(function (response){
+    return(response.json());
+    }).then(function(data){
     
-    console.log('data :>> ', data);
-    let mainActor = data.Actors.split(",")[0];
-    $('#Movie-Title').text(data.Title);
-    $('#Director').text(data.Director);
-    $('#Main-Actor').text(mainActor);
-    $('#Year').text(data.Year);
-    $('#Genre').text(data.Genre);
+        movieName = data.Title;
+        mainActor = data.Actors.split(",")[0];
+        
+        $('#Movie-Title').text(data.Title);
+        $('#Director').text(data.Director);
+        $('#Main-Actor').text(mainActor);
+        $('#Year').text(data.Year);
+        $('#Genre').text(data.Genre);
 
-    var posterString = '<img src="' + data.Poster +'"' + ' alt="Poster" width="auto" height="180px"' + '/>';
-    $('#PosterContainer').append(posterString);
-            
+        var posterString = '<img src="' + data.Poster +'"' + ' alt="Poster" width="auto" height="180px"' + '/>';
+        $('#PosterContainer').append(posterString);
+
+        getActorId(movieName);
 }); 
+    
+}
+
+function getActorId(movie){
+    var url = "https://imdb8.p.rapidapi.com/title/find?q="+movie;
+    var options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '2cb15b5d0bmsh9a58fa38194342bp15d719jsn583d89ed735c',
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+
+    fetch(url, options)
+    .then(function(response){
+        return(response.json());
+    }).then(function(data){
+        console.log("callIMDB for movie", data); 
+        console.log('id :>> ', data.results[0].principals[0].id);  
+        var actorIdRaw = data.results[0].principals[0].id;
+        const start = actorIdRaw.search("nm");
+        const end = actorIdRaw.length - 1;
+        var actorId = actorIdRaw.substring(start,end);
+        getActorBio(actorId)
+    });
+}
+
+function getActorBio(actorId){
+    url = "https://imdb8.p.rapidapi.com/actors/get-bio?nconst=" + actorId;
+    var options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '2cb15b5d0bmsh9a58fa38194342bp15d719jsn583d89ed735c',
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+
+    fetch(url, options)
+    .then(function(response){
+        return(response.json());
+    }).then(function(data){
+        var actorBioRaw = data.miniBios[0].text;
+        const lim = actorBioRaw.search("\n");
+        var actorBio = actorBioRaw.substring(0,lim);
+        var actorName = data.name;
+        var actorRealName = data.realName;
+
+        $('#PersonName').text(actorName);
+        $('#PersonBio').text(actorBio);
+
+    }).catch(err => console.error(err));
 
 }
+
 
 $('#PosterContainer').empty();
